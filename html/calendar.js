3f267dba03a8e9e5d0d629e16a9a4e656fe8ff97
NAHyWo <nahw0403@gmail.com>
2022-05-26 05:05:34 +0900

    달력 수정(2022.05.26) <JS> : 나현우

--- html/calendar.js
+++ html/calendar.js
@@ -1,19 +1,19 @@
 var today = new Date();         
 var date = new Date();
 	
-	//이전달
+	//������
 	function beforeMonth() { 
 		today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
 		showCalendar(); 
 	}
 	
-	//다음달
+	//������
 	function nextMonth() {
 		today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
 		showCalendar();
 	}
 	
-	//오늘선택
+	//���ü���
 	function thisMonth(){
 		today = new Date();
 		showCalendar();
@@ -25,20 +25,7 @@ var date = new Date();
 		var lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0); 
 		var tbcal = document.getElementById("calendar"); 
 		var yearmonth = document.getElementById("yearmonth"); 
-		yearmonth.innerHTML = today.getFullYear() + "년 "+ (today.getMonth() + 1) + "월"; 
-
-		if(today.getMonth()+1==12) {
-			before.innerHTML="<";
-			next.innerHTML=">";
-		}
-		else if(today.getMonth()+1==1) {
-			before.innerHTML="<";
-			next.innerHTML=">";
-		}
-		else {
-			before.innerHTML="<";
-			next.innerHTML=">";
-		}
+		yearmonth.innerHTML = today.getFullYear() + "�� "+ (today.getMonth() + 1) + "��"; 
 
 
 		while (tbcal.rows.length > 2) {
@@ -54,13 +41,14 @@ var date = new Date();
         }else{
             var dayCheck = nMonth.getDay();
         }
-		for (i = 0; i < (dayCheck-1); i++) {
+		
+		for (i = 0; i < nMonth.getDay(); i++) {
 			cnt = cnt + 1;	
 			cell = row.insertCell();
 		}
 
 
-		// 달력 출력
+		// �޷� ���
 		for (i = 1; i <= lastDate.getDate(); i++)
 		{ 
 			cell = row.insertCell();
@@ -77,18 +65,8 @@ var date = new Date();
 			cell.innerHTML = str;
 			
 			cnt = cnt + 1;
-			if (cnt % 7 == 6) { //토요일
-				var str="";
-				str += "<div>"+i+"</div>";
-                if(i<10){
-                    var day = "0"+i; 
-                }else{
-                    var day = i;
-                }       	
-				str += "<div id='"+day+"'>"+"</div>";
-				cell.innerHTML = str;                
-			}
-			if (cnt % 7 == 0) { //일요일
+
+			if (cnt % 7 == 0) { //�����
 				var str="";
 				str += "<div>"+i+"</div>";
                 if(i<10){
@@ -97,18 +75,31 @@ var date = new Date();
                     var day = i;
                 }                	
 				str += "<div id='"+day+"'>"+"</div>";
-				cell.innerHTML = str;
+				cell.innerHTML = "<font color = #3737FF>" + str;
 				row = calendar.insertRow();
 			}
+			if (cnt % 7 == 1) { //�Ͽ���
+				var str="";
+				str += "<div>"+i+"</div>";
+                if(i<10){
+                    var day = "0"+i; 
+                }else{
+                    var day = i;
+                }       	
+				str += "<div id='"+day+"'>"+"</div>";
+				cell.innerHTML = "<font color = #FF3737>" + str;                
+			}
 			
-			
-			  
+			if(today.getFullYear()==date.getFullYear()&&today.getMonth()==date.getMonth()&&i==date.getDate()) 
+            {
+				cell.innerHTML = "<font color = #AFAF7F>" + "<font size = 5px>" + str;
+            }
 		}
 		
 		var tdId = "22"; 
 		var str = "";
-		str += "넷플릭스  ";
-		str += "왓차 ";
+		str += "���ø���  ";
+		str += "���� ";
 		document.getElementById(tdId).innerHTML = str;
 	}
 
